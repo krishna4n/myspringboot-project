59c814bcfb6aa78080e3076dff71502e8c24511f
Gopikrishna <krihna4n@gmail.com>
2024-01-07 19:04:19 +0530

    second update

--- src/main/java/com/example/myproject/model/Author.java
+++ src/main/java/com/example/myproject/model/Author.java
@@ -3,8 +3,10 @@ package com.example.myproject.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -22,7 +24,8 @@ public class Author {
     @Column(name = "authorname")
     private String authorName;
 
-    @ManyToMany(mappedBy = "books")
+    //@ManyToMany(mappedBy = "authors", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @ManyToMany(mappedBy = "authors")
     private List<Book> books = new ArrayList<>(); 
 
     public Author(){}
